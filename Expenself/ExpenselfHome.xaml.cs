2216a5fe8fcfcd1f3ce1d13db06e5d111d50b7d0
czltking <lubula@126.com>
2020-08-06 08:42:16 +0800

    First commit

---
+++ Expenself/ExpenselfHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expenself
+{
+    /// <summary>
+    /// ExpenselfHome.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExpenselfHome : Page
+    {
+        public ExpenselfHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Expensereportpage expensereportpage = new Expensereportpage(this.peopleListBox.SelectedItem);
+          //  ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expensereportpage);
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,6 @@
+1.Expenself  是利用VS2019创建的一个Demo
+目的为了熟悉在VS2019的环境下创建WPF工程
+主要演示以下几个内容：
+1.Style - Setter  样式模板设置
+2.Grid 布局设置
+3.DataList的用法 和数据绑定
